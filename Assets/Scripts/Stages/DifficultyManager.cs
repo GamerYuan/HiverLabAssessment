1fc12a9317b0910cdafd5f164a756bd8c863c00d
GamerYuan <f2pplayer0801@gmail.com>
2024-02-04 19:38:45 +0800

    Balance changes

--- Assets/Scripts/Stages/DifficultyManager.cs
+++ Assets/Scripts/Stages/DifficultyManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DifficultyManager
 {
     private static float baseDifficulty = 1, difficulty = 1;
-    private static float maxDifficulty = 10;
+    private static float maxDifficulty = 3;
 
     public static float Difficulty { get => difficulty; private set => difficulty = value; }
 
